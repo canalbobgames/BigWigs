30e2edae6e82f270e5206d73b47e61e954d5c5cb
canalbobgames <50850275+canalbobgames@users.noreply.github.com>
2019-09-10 14:22:58 -0300

    Update ptBR.lua

--- BattleOfDazaralor/Locales/ptBR.lua
+++ BattleOfDazaralor/Locales/ptBR.lua
@@ -8,8 +8,8 @@ if L then
 	L.punisher = "Castigador Rastari"
 	L.vessel = "Receptáculo de Bwonsamdi"
 
-	--L.victim = "%s stabbed YOU with %s!"
-	--L.witness = "%s stabbed %s with %s!"
+	--L.victim = "%s apunhalou VOCE com %s!"
+	--L.witness = "%s apunhalou %s com %s!"
 end
 
 L = BigWigs:NewBossLocale("Champion of the Light Horde", "ptBR")
@@ -27,12 +27,12 @@ if L then
 	--L.custom_on_fixate_plates = "Stalking icon on Enemy Nameplate"
 	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is stalking on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
 
-	--L.absorb = "Absorb"
+	--L.absorb = "Absorver"
 	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.cast = "Cast"
+	--L.cast = "Conjurar"
 	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 
-	--L.interrupted_after = "%s interrupted by %s (%.1f seconds remaining)"
+	--L.interrupted_after = "%s interrompido por %s (%.1f segundos restantes)"
 end
 
 L = BigWigs:NewBossLocale("Jadefire Masters Alliance", "ptBR")
@@ -40,47 +40,47 @@ if L then
 	--L.custom_on_fixate_plates = "Stalking icon on Enemy Nameplate"
 	--L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is stalking on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
 
-	--L.absorb = "Absorb"
+	--L.absorb = "Absorver"
 	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.cast = "Cast"
+	--L.cast = "Conjurar"
 	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 
-	--L.interrupted_after = "%s interrupted by %s (%.1f seconds remaining)"
+	--L.interrupted_after = "%s interrompido por %s (%.1f segundos restantes)"
 end
 
 L = BigWigs:NewBossLocale("Opulence", "ptBR")
 if L then
-	--L.room = "Room (%d/8)"
-	--L.no_jewel = "No Jewel:"
+	--L.room = "Sala (%d/8)"
+	--L.no_jewel = "Sem Jóia:"
 
 	--L.custom_on_fade_out_bars = "Fade out stage 1 bars"
 	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the construct which isn't in your hall during stage 1."
 
 	--L.custom_on_hand_timers = "The Hand of In'zashi"
 	--L.custom_on_hand_timers_desc = "Show warnings and bars for The Hand of In'zashi's abilities."
-	--L.hand_cast = "Hand: %s"
+	--L.hand_cast = "Mão: %s"
 
 	--L.custom_on_bulwark_timers = "Yalat's Bulwark"
 	--L.custom_on_bulwark_timers_desc = "Show warnings and bars for Yalat's Bulwark's abilities."
-	--L.bulwark_cast = "Bulwark: %s"
+	--L.bulwark_cast = "Baluarte: %s"
 end
 
 L = BigWigs:NewBossLocale("Conclave of the Chosen", "ptBR")
 if L then
 	-- L.custom_on_fixate_plates = "Mark of Prey icon on Enemy Nameplate"
 	-- L.custom_on_fixate_plates_desc = "Show an icon on the target nameplate that is fixating on you.\nRequires the use of Enemy Nameplates. This feature is currently only supported by KuiNameplates."
-	--L.killed = "%s killed!"
+	--L.killed = "%s morto!"
 	--L.count_of = "%s (%d/%d)"
 end
 
 L = BigWigs:NewBossLocale("King Rastakhan", "ptBR")
 if L then
-	--L.leap_cancelled = "Leap Cancelled"
+	--L.leap_cancelled = "Salto Cancelado"
 end
 
 L = BigWigs:NewBossLocale("High Tinker Mekkatorque", "ptBR")
 if L then
-	--L.gigavolt_alt_text = "Bomb"
+	--L.gigavolt_alt_text = "Bomba"
 
 	--L.custom_off_sparkbot_marker = "Spark Bot Marker"
 	--L.custom_off_sparkbot_marker_desc = "Mark Spark Bots with {rt4}{rt5}{rt6}{rt7}{rt8}."
@@ -94,7 +94,7 @@ end
 
 L = BigWigs:NewBossLocale("Stormwall Blockade", "ptBR")
 if L then
-	--L.killed = "%s killed!"
+	--L.killed = "%s morto!"
 
 	--L.custom_on_fade_out_bars = "Fade out stage 1 bars"
 	--L.custom_on_fade_out_bars_desc = "Fade out bars which belong to the boss who isn't active on your boat in stage 1."
@@ -102,8 +102,8 @@ end
 
 L = BigWigs:NewBossLocale("Lady Jaina Proudmoore", "ptBR")
 if L then
-	--L.starbord_ship_emote = "A Kul Tiran Corsair approaches on the starboard side!"
-	--L.port_side_ship_emote = "A Kul Tiran Corsair approaches on the port side!"
+	--L.starbord_ship_emote = "Um Corsário Kul Tireno se aproxima pelo lado de estibordo!"
+	--L.port_side_ship_emote = "Um Corsário Kul Tireno se aproxima pelo lado do porto!"
 
 	--L.starbord_txt = "Right Ship" -- starboard
 	--L.port_side_txt = "Left Ship" -- port
