7c5965b83811731743cc6f5247c534d2c3eaf5e8
canalbobgames <50850275+canalbobgames@users.noreply.github.com>
2019-09-10 16:05:04 -0300

    Update ptBR.lua

--- Azeroth/Locales/ptBR.lua
+++ Azeroth/Locales/ptBR.lua
@@ -1,5 +1,5 @@
 local L = BigWigs:NewBossLocale("Warbringer Yenajz", "ptBR")
 if not L then return end
 if L then
-	--L.tear = "Você ficou em um Rasgo da Realidade"
+	L.tear = "Você ficou em um Rasgo da Realidade"
 end
