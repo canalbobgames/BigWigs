efd530e3fa8e984747a92a9ce80e57c30947ab3d
canalbobgames <50850275+canalbobgames@users.noreply.github.com>
2019-09-10 14:28:19 -0300

    Update ptBR.lua

--- CrucibleOfStorms/Locales/ptBR.lua
+++ CrucibleOfStorms/Locales/ptBR.lua
@@ -1,10 +1,10 @@
 local L = BigWigs:NewBossLocale("The Restless Cabal", "ptBR")
 if not L then return end
 if L then
-	--L.absorb = "Absorb"
+	--L.absorb = "Absorver"
 	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
-	--L.cast = "Cast"
+	--L.bubble = "Bolha" -- Custody of the Deep Bubble
+	--L.cast = "Conjurar"
 	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 end
 
@@ -13,15 +13,15 @@ if L then
 	--L.custom_on_stop_timers = "Always show ability bars"
 	--L.custom_on_stop_timers_desc = "Uunat can delay some of his abilities. When this option is enabled, the bars for those abilities will stay on your screen."
 
-	--L.absorb = "Absorb"
+	--L.absorb = "Absorver"
 	--L.absorb_text = "%s (|cff%s%.0f%%|r)"
-	--L.bubble = "Bubble" -- Custody of the Deep Bubble
-	--L.cast = "Cast"
+	--L.bubble = "Bolha" -- Custody of the Deep Bubble
+	--L.cast = "Conjurar"
 	--L.cast_text = "%.1fs (|cff%s%.0f%%|r)"
 
-	--L.void = "Void" -- Unstable Resonance: Void
-	--L.ocean = "Ocean" -- Unstable Resonance: Ocean
-	--L.storm = "Storm" -- Unstable Resonance: Storm
+	--L.void = "Vazio" -- Unstable Resonance: Void
+	--L.ocean = "Oceano" -- Unstable Resonance: Ocean
+	--L.storm = "Tempestade" -- Unstable Resonance: Storm
 
 	--L.custom_on_repeating_resonance_yell = "Repeating Relics of Power Yell"
 	--L.custom_on_repeating_resonance_yell_desc = "Spam a yell stating which relic you are holding during Unstable Resonance."
